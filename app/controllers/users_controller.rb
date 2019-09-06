6a57ee4601395de9bfdc8f2ab7aa676627aecd33
yusuke.honda <14au064f@al.rikkyo.ac.jp>
2019-09-06 08:30:10 +0000

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      flash[:success] = "会員登録が完了しました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "会員登録に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
