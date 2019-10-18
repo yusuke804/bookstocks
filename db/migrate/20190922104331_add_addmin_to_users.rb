15c3849f4996667274a0e2e3190c5d5ddd2bf6b8
yusuke.honda <14au064f@al.rikkyo.ac.jp>
2019-10-18 13:04:38 +0000

    デプロイ前最終コミット

--- db/migrate/20190922104331_add_addmin_to_users.rb
+++ db/migrate/20190922104331_add_addmin_to_users.rb
@@ -1,5 +1,5 @@
-class AddAdminToUsers < ActiveRecord::Migration[5.2]
+class AddAddminToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :admin, :boolean, default: false, null: false
+    add_column :users, :addmin, :boolean, default: false, null: false
   end
 end
